e18c7a67d52cb17fd30ec98086947d2565438f7d
Nishant Tanwar <nishantt12@gmail.com>
2019-10-07 13:53:08 +0530

    added doia

--- app/src/main/java/com/foodora/adapter/CatalogAdapter.kt
+++ app/src/main/java/com/foodora/adapter/CatalogAdapter.kt
@@ -1,11 +1,17 @@
 package com.foodora.adapter
 
+import android.graphics.Bitmap
 import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Transition
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.SimpleTarget
 import com.foodora.R
 import com.foodora.model.catalog.Product
 import kotlinx.android.synthetic.main.item_catalog.view.*
@@ -37,8 +43,25 @@ class CatalogAdapter(private val items: List<Product>, private val listener: ((P
                 listener.invoke(product)
             }
             Glide.with(itemView)
+                    .asBitmap()
                     .load(product?.image)
-                    .into(itemView.img_catalog)
+                    .into(object : SimpleTarget<Bitmap>() {
+
+                        override fun onResourceReady(resource: Bitmap, transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?) {
+                            Palette.Builder(resource).generate { it?.let { palette ->
+                                val dominantColor = palette.getDominantColor(ContextCompat.getColor(itemView.context, R.color.colorAccent))
+                               Timber.d("dominantColor: $dominantColor")
+                                itemView.main_layout.setBackgroundColor(dominantColor)
+
+                            } }
+
+                            itemView.img_catalog.setImageBitmap(resource)
+                        }
+
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                        }
+                    })
 
             itemView.tv_name.text = product?.name
             Timber.d("originalPrice: 0")
@@ -50,7 +73,9 @@ class CatalogAdapter(private val items: List<Product>, private val listener: ((P
                 itemView.tv_price_original.visibility = View.VISIBLE
                 val originalPrice = "${product?.price?.original} ${product?.price?.currency}"
                 Timber.d("originalPrice: $originalPrice")
-                itemView.tv_price_original.setPaintFlags(itemView.tv_price_original.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG.inv())
+                itemView.tv_price_original.paintFlags = itemView.tv_price_original.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+
+//                itemView.tv_price_original.setPaintFlags(itemView.tv_price_original.getPaintFlags() or Paint.STRIKE_THRU_TEXT_FLAG.inv())
                 itemView.tv_price_original.text = originalPrice
             }
             itemView.tv_price.text = "${product?.price?.current} ${product?.price?.currency}"
