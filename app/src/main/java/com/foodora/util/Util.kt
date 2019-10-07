df97eb25b64f607f007ebdfd59bbde9ed8a99224
Nishant Tanwar <nishantt12@gmail.com>
2019-10-07 13:36:58 +0530

    removed unused files

--- app/src/main/java/com/foodora/util/Util.kt
+++ app/src/main/java/com/foodora/util/Util.kt
@@ -3,7 +3,6 @@ package com.foodora.util
 import android.content.Context
 import android.text.TextUtils
 import android.util.Log
-import android.util.Patterns
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
 import com.foodora.BuildConfig
@@ -19,19 +18,6 @@ object Util {
     return text == null || TextUtils.isEmpty(text) || text.length == 0
   }
 
-  fun isValidEmail(email: String?): Boolean {
-    if (!isNullOrEmptyString(email)) {
-      return Patterns.EMAIL_ADDRESS.matcher(email).matches()
-    }
-    return false
-  }
-
-  fun isValidMobileNo(mobile: String?): Boolean {
-    if (!isNullOrEmptyString(mobile) || mobile?.length == 10) {
-      return Patterns.PHONE.matcher(mobile).matches()
-    }
-    return false
-  }
 
   fun showToast(message: String?, context: Context?) {
     Toast.makeText(context, message, LENGTH_SHORT).show()
