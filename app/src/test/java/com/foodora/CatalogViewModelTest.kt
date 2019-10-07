88bf96737f3f9fde4c41591d9c92b748e2822462
Nishant Tanwar <nishantt12@gmail.com>
2019-10-07 12:52:48 +0530

    fixed small issues

---
+++ app/src/test/java/com/foodora/CatalogViewModelTest.kt
@@ -0,0 +1,22 @@
+package com.foodora
+
+import com.foodora.repository.CatalogRepository
+import com.foodora.repository.api.CatalogService
+import com.foodora.viewModels.CatalogViewModel
+import org.junit.Before
+import org.mockito.Mockito.mock
+
+class CatalogViewModelTest {
+
+    private lateinit var viewModel: CatalogViewModel
+    private lateinit var catalogRepository: CatalogRepository
+
+
+//    private val catalogService = mock<CatalogService>()
+
+
+    @Before
+    fun init(){
+
+    }
+}
